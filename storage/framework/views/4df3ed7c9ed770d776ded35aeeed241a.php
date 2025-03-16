37102eb302aaede75198d1833acb09498a112dea
gomarcd <git@gomarcd.dev>
2025-03-16 17:59:01 -0600

    rm storage & tests

--- storage/framework/views/4df3ed7c9ed770d776ded35aeeed241a.php
+++
@@ -1,6 +0,0 @@
-
-<ui-close data-flux-modal-close>
-    <?php echo e($slot); ?>
-
-</ui-close>
-<?php /**PATH /Users/md/dev/tradesimple/vendor/livewire/flux/src/../stubs/resources/views/flux/modal/close.blade.php ENDPATH**/ ?>
